50aa027029f5507549d47238006cb2e8ee47381c
BrunoMNDantas <bmndantas@gmail.com>
2016-03-21 15:57:32 +0000

    Spell check and bug on trigger fixed

--- src/main/java/jfxwfutils/Event.java
+++ src/main/java/jfxwfutils/Event.java
@@ -20,38 +20,39 @@
 package jfxwfutils;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.function.Consumer;
 
 
 public class Event<T> {
 	
-	private final Map<Object, Consumer<T>> listners;
+	private final Map<Object, Consumer<T>> listeners;
 	
 	public Event() {
-		this.listners = new HashMap<>();
+		this.listeners = new HashMap<>();
 	}
 
-	public void addListner(Consumer<T> listner){
-		listners.put(listner, listner);
+	public void addListener(Consumer<T> listener){
+		listeners.put(listener, listener);
 	}
 	
-	public void removeListner(Consumer<T> listner) {
-		listners.remove(listner);
+	public void removeListener(Consumer<T> listener) {
+		listeners.remove(listener);
 	}
 	
-	public void addListner(Runnable listner){
-		Consumer<T> consumer = (t)->listner.run();
-		listners.put(listner, consumer);
+	public void addListener(Runnable listener){
+		Consumer<T> consumer = (t)->listener.run();
+		listeners.put(listener, consumer);
 	}
 	
-	public void removeListner(Runnable listner){
-		listners.remove(listner);
+	public void removeListener(Runnable listener){
+		listeners.remove(listener);
 	}
-	
+
 	public void trigger(T t){
-		for(Object key : listners.keySet())
-			listners.get(key).accept(t);
+		for(Object key : new LinkedList<>(listeners.keySet()))
+			listeners.get(key).accept(t);
 	}
 	
 }
